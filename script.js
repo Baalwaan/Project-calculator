e5f70873495aa391f69f529e6baf8498b96e96ee
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-02-13 14:29:56 +0000

    new approach test

--- script.js
+++ script.js
@@ -3,43 +3,91 @@ currentNum = '';
 
 
 display = document.getElementById('display');
-// this is an array like object
 numbers = document.querySelectorAll('.numbers');
-// turns numbers into a real array so we can add events
-// numbers = [...numbers];
-
 operators = document.querySelectorAll('.operator');
 
 
-calculate = document.getElementById('equals').addEventListener('click', function(){
-display.innerHTML = eval(display);
+equals = document.getElementById('equals').addEventListener('click', function(){
+display.innerHTML = eval(display.innerHTML);
 })
+
+decimal = document.getElementById('decimal').addEventListener('click', decimalFunc);
+
+clear = document.getElementById('clear').addEventListener('click', function(){
+	resetCurrentNum();
+	display.innerHTML = '0'})
+
+
+
+
 
 
 let showOnScreen = function(){
+if(currentNum.match(/\W+/g)) {
+	resetCurrentNum();
+}
+// when 0 is on screen and user presses 0 it does not do anything
+if(display.innerHTML == 0 && this.value == 0){console.log('am zero')}
+// current number checker
+if(currentNum == 0 && this.value == 0){console.log('I am also a zero')}
+// in initial stge when 0 is showing and user presses a number it removes the 0 first
 
-currentNum = this.value
-
-if(display.innerHTML == 0 && currentNum == 0){''}
-
-else if(display.innerHTML == 0 && currentNum > 0){
-	display.innerHTML = '';
-	output = display.innerHTML+= currentNum
+else if(display.innerHTML == 0 && this.value > 0){
+	
+	display.innerHTML='';
+	currentNum+=this.value;
+	display.innerHTML+=this.value;
+}
+// just adds number to current number
+else{
+	currentNum+=this.value
+	display.innerHTML+= this.value;
 }
 
-else{output = display.innerHTML+= currentNum}
+}
 
+
+
+function resetCurrentNum(){
+	currentNum = '';
+
+}
+let operatorFunc = function(){
+
+	if(display.innerHTML[display.innerHTML.length-1].match(/\W+/g)){
+		display.innerHTML = display.innerHTML.replace(display.innerHTML[display.innerHTML.length-1], this.value)
+		
+
+	}
+
+	else{
+		currentNum+= this.value;
+		display.innerHTML+=this.value;
+
+	}
+}
+
+
+
+function decimalFunc(){
+	// checks to see if current number has a dot. if it has it wont do nothing 
+	if(currentNum.includes(this.value)){
+		alert('You already have a decimal point in your number!')
+	}
+
+// otherwise 
+	else{
+		currentNum += this.value;
+		display.innerHTML += this.value;
+		console.log(this.value)
+	}
+	display.innerHTML;
 
 }
 
 
 
-let operatorFunc = function{
-currentNum = '';
-
-
-}
 
 
 numbers.forEach(e=>e.addEventListener('click', showOnScreen))
-operators.forEach(e=>e.addEventListener('click', showOnScreen))
+operators.forEach(e=>e.addEventListener('click', operatorFunc))
