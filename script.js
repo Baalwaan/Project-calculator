922e48d63151edb341b1a5a4a0ebcb30ba0dbdaf
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-02-16 14:34:16 +0000

    unexpected result bug fixed (convert int back to string number fixed this). added also currentNum variable for func decimalFunc to use

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+total = 0;
+
+
 display = document.getElementById('display');
 numbers = document.querySelectorAll('.numbers');
 operators = document.querySelectorAll('.operator');
 
-
 equals = document.getElementById('equals').addEventListener('click', function(){
-display.innerHTML = eval(display.innerHTML).toFixed(2);
+	// currentNum = ''
+total += eval(display.innerHTML);
+arr = (total).toString().split('');
+display.innerHTML = (total).toString();
+
 })
+
+currentNum = '';
 
 decimal = document.getElementById('decimal').addEventListener('click', decimalFunc);
 
@@ -17,8 +25,6 @@ clear = document.getElementById('clear').addEventListener('click', function(){
 
 var arr = [0];
 
-
-
 let showOnScreen = function(){
 // when 0 is on screen and user presses 0 it does not do anything
 if(display.innerHTML == 0 && this.value == 0){
@@ -28,14 +34,16 @@ if(display.innerHTML == 0 && this.value == 0){
 // in initial stge when 0 is showing and user presses a number it removes the 0 first
 else if(display.innerHTML == 0 && this.value > 0){
 	
+	currentNum += this.value;
 	clearDisplay();
 	arr.shift();
 	arr.push(this.value);
 	display.innerHTML = arr.join('');
+
 }
 // just adds number to current number
 else{
-	
+	currentNum += this.value;
 	clearDisplay();
 	arr.push(this.value);
 	display.innerHTML+= arr.join('')
@@ -43,17 +51,21 @@ else{
 }
 
 let operatorFunc = function(){
+currentNum = '';
 clearDisplay();
+
 
 	if(arr[arr.length-1].match(/\W+/g)){
 		
-		arr.splice(-1, 1, this.value);		
+		arr.splice(-1, 1, this.value);	
+		console.log(arr.join(''))	
 		display.innerHTML = arr.join('');
 			}
 
 	else{
 
 		arr.push(this.value);	
+		console.log(arr.join(''))
 		display.innerHTML+= arr.join('');
 			}
 }
@@ -61,6 +73,7 @@ clearDisplay();
 
 function clearDisplay(){
 	display.innerHTML = '';
+	total = 0;
 }
 
 
@@ -73,13 +86,20 @@ function decimalFunc(){
 
 // otherwise 
 	else{
+		clearDisplay();
 		currentNum += this.value;
-		display.innerHTML += this.value;
-		console.log(this.value)
+		arr.push(this.value);
+		// console.log(arr.join(''))
+		display.innerHTML = arr.join('');
+		
 	}
-	display.innerHTML;
-
+	
 }
+
+function resetCurrentNum(){
+	currentNum = '';
+}
+
 
 
 
